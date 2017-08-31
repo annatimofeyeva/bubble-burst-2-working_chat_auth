dc7b17ac5e0d07cd520a3162992327a5b50bde13
Anna Kuznetsova and Anna Timofeeva <anna+anna@>
2017-08-31 14:27:27 -0700

    add chat with gmail auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,67 @@ import { Component, OnInit} from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { MarsPhotosService } from './mars-photos.service';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import * as firebase from 'firebase/app';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ MarsPhotosService ]
+  providers: [ MarsPhotosService,AngularFireAuth ]
 })
 export class AppComponent {
 
   photos: any[]=null;
   noPhotos: boolean=false;
 
-  constructor(private marsRoverPhotos: MarsPhotosService) { }
+  private isLoggedIn: Boolean;
+  private userName: String;
+
+
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+
+  constructor(private marsRoverPhotos: MarsPhotosService, public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+
+    this.items = af.list('/messages', {
+          query: {
+            limitToLast: 50
+          }
+        });
+
+        this.user = this.afAuth.authState;
+        this.user.subscribe(user =>  {
+              console.log(user);
+            });
+
+    this.user.subscribe(user => {
+          if (user == null) {
+            this.isLoggedIn = false;
+          } else {
+            this.isLoggedIn = true;
+            this.userName = user.displayName;
+          }
+        });
+
+  }
+
+  login() {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+      this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string) {
+      this.items.push({ message: desc});
+      this.msgVal = '';
+  }
 
   ngOnInit() {
     this.noPhotos = false;
